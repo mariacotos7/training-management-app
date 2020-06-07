39a87828084b89b6715785aa2a0c978dce7d49a9
Maria Cotos <maria.cotos7@yahoo.com>
2020-06-07 16:49:37 +0300

    added functionalities

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TrainingManagement.MixProject do
     [
       app: :trainingManagement,
       version: "0.1.0",
-      elixir: "~> 1.10.3",
+      elixir: "~> 1.9.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
